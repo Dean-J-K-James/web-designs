5024523f5acf006cf5fb81efbd7f1b770dd65b62
Dean James <dean.j.k.james@gmail.com>
2023-02-26 15:48:40 +0000

    Added slick to template

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -1,12 +1,10 @@
-$(document).ready(function () {
-    $('.slick').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: false,
-        prevArrow: false,
-        nextArrow: false,
-        centerMode: true
-    });
+$('.slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: false,
+    prevArrow: false,
+    nextArrow: false,
+    centerMode: true
 });
